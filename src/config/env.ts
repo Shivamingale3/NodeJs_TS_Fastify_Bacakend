1f82405198c030cfbd011a065f623508761d2630
Shivamingale3 <shivamingale3@gmail.com>
2025-12-24 17:47:43 +0530

    Database Setup: Schema and Connection

---
+++ src/config/env.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  PORT: z.string().transform(Number).default("3000"),
+  HOST: z.string().default("0.0.0.0"),
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  DATABASE_URL: z.string(),
+  LOKI_HOST: z.string().url(),
+  JWT_SECRET: z.string().min(10),
+  ADMIN_SECRET: z.string().optional(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error("❌ Invalid environment variables ❌");
+  console.error(_env.error.format());
+  process.exit(1);
+}
+
+export const env = _env.data;
